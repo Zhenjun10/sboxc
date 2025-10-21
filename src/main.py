2dbf390883c163cd923392f19dc8d596c2be100f
cheremace <cheremace@gmail.com>
2025-10-21 16:15:10 +0800

    chore: use uv to manage project

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from configer import Configer
 
 
-SUB_URL = 'https://tqt-pwezahrw.tutunode.com/gateway/taoqitu?token=4bbabf8f4ca677564b30ef93778164d9'
+SUB_URL = ''
 
 cfg = Configer(SUB_URL)
 cfg.inbounds.pop()
